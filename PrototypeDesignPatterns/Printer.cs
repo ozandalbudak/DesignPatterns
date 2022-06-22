aef508485176bd8ba6da85bb9805c7e73c58d405
Ozan DALBUDAK <odalbudak@isbak.istanbul>
2022-06-22 21:47:22 +0300

    PrototypeDesignPatterns eklendir

---
+++ PrototypeDesignPatterns/Printer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeDesignPatterns
+{
+     class Printer : PrinterPrototype
+    {
+        public byte Address { get; set; }
+        public string Name { get; set; }
+        public bool ColorCartridge { get; set; }
+        public bool BlackCartridge { get; set; }
+        public int ResolutionDpi { get; set; }
+        public string PaperSize { get; set; }
+        public Printer(byte address, string name, bool colorCartridge,
+        bool blackCartridge, int resolution, string paperSize)
+        {
+            Address = address;
+            Name = name;
+            ColorCartridge = colorCartridge;
+            BlackCartridge = blackCartridge;
+            ResolutionDpi = resolution;
+            PaperSize = paperSize;
+            System.Threading.Thread.Sleep(1000);
+        }
+        public override PrinterPrototype Clone()
+        {
+            // Klonlama işlemi. Gelen nesnenin bir kopyası alınıp sonuç             olarak döndürülüyor. 
+ return (PrinterPrototype)this.MemberwiseClone();
+        }
+    }
+}
--- PrototypeDesignPatterns/Program.cs
+++ PrototypeDesignPatterns/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PrototyopeDesignPatterns
+namespace PrototypeDesignPatterns
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Prototype model nesne oluşturuluyor. 
+            Printer printer = new Printer(0x0F, "Hp", true, true, 600,
+           "A4");
+            for (int i = 0; i < 20; i++)
+            {
+                PrintDocument((Printer)printer.Clone(), i.ToString());
+                Console.WriteLine(Convert.ToString(i + 1) + " nolu sayfa basıldı"); 
+            }
+            Console.WriteLine("Yazdırma tamamlandı...");
+            Console.ReadLine();
+        }
+        static void PrintDocument(Printer printer, string content)
+        {
+            // Print işlemi yapan metod. 
         }
     }
 }

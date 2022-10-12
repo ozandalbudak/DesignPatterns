20060d67c54c031aa6e413589b66a9580d97e325
Ozan DALBUDAK <odalbudak@isbak.istanbul>
2022-10-12 20:42:34 +0300

    AddInterpreterDesignPattern

--- BehavioralDesignPatterns/CommandDesignPattern/CommandDesignPattern/Program.cs
+++ BehavioralDesignPatterns/CommandDesignPattern/CommandDesignPattern/Program.cs
@@ -10,6 +10,8 @@ namespace CommandDesignPattern
     {
         static void Main(string[] args)
         {
+            
+
         }
     }
 }
--- BehavioralDesignPatterns/InterpreterDesignPattern/InterpreterDesignPattern/Program.cs
+++ BehavioralDesignPatterns/InterpreterDesignPattern/InterpreterDesignPattern/Program.cs
@@ -10,6 +10,20 @@ namespace InterpreterDesignPattern
     {
         static void Main(string[] args)
         {
+            string roman = "MCMXXVIII";
+            Context context = new Context(roman);
+            List<Expression> tree = new List<Expression>();
+            tree.Add(new ThousandExpression());
+            tree.Add(new HundredExpression());
+            tree.Add(new TenExpression());
+            tree.Add(new OneExpression());
+            foreach (Expression exp in tree)
+            {
+                exp.Interpret(context);
+            }
+            Console.WriteLine("{0} = {1}",
+            roman, context.Output);
+            Console.ReadKey();
         }
     }
 }
